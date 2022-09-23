55256d18170a9d6ccc9a56668a5c5ca60c14f05a
VadimStarv <105434892+VadimStarv@users.noreply.github.com>
2022-09-23 21:49:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let headerList = document.querySelector('.special__li');
+headerList.addEventListener('click',function(){
+  let ourHeader = document.querySelector('.about__header');
+  ourHeader.classList.toggle('active');
+})
+
+
+
